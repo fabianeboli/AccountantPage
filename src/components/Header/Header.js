2bb3bc481558a5e76b8da91a71a8d4b64f1f2eb4
Fabian Eboli <fabiane@disroot.org>
2019-03-16 14:34:38 +0100

    Corrected Responsiveness

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,12 +7,14 @@ import { faArrowCircleDown } from '@fortawesome/free-solid-svg-icons'
 export default () => {
   return (
     <section className={styles.image}>
+      <div className={styles.text}>
         <div className={styles.companyName}>VAT-PIT</div>
         <div className={styles.ownersName}>Alicja Ziółkowska</div>
-        <a href='#about'>
-        <FontAwesomeIcon className={styles.arrow} icon={faArrowCircleDown} size='4x' color='RGBA(255,250,250,0.3)'/>
-        </a>
-      <NavBar/>
+      </div>
+      <a href='#about'>
+        <FontAwesomeIcon className={styles.arrow} icon={faArrowCircleDown} size='4x' color='RGBA(255,250,250,0.3)' />
+      </a>
+        <NavBar />
     </section>
   )
 }
